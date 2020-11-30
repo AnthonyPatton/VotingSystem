1c94162a058a245976f4c8b11e0a4dd09c4f18e0
Anthony Patton <anthonyalanpatton@gmail.com>
2020-11-30 04:08:53 -0800

    Adding Vote and Comment join tables

---
+++ VotingSystem/src/main/java/com/votingsystem/domain/CommentId.java
@@ -0,0 +1,32 @@
+package com.votingsystem.domain;
+
+import java.io.Serializable;
+
+import javax.persistence.Embeddable;
+import javax.persistence.ManyToOne;
+
+@Embeddable
+public class CommentId implements Serializable{
+
+	
+	private static final long serialVersionUID = -8317669029165868874L;
+	private User user;
+	private Feature feature;
+	
+	@ManyToOne
+	public User getUser() {
+		return user;
+	}
+	public void setUser(User user) {
+		this.user = user;
+	}
+	
+	@ManyToOne
+	public Feature getFeature() {
+		return feature;
+	}
+	public void setFeature(Feature feature) {
+		this.feature = feature;
+	}
+	 
+}
